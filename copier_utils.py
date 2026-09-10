4724f133e5bf98894e93931af538d769e1580045
Rayn <raynho@proton.me>
2026-09-10 02:41:03 +0200

    added template list plus tools to find and add new templates

--- copier_utils.py
+++ copier_utils.py
@@ -1,7 +1,7 @@
+import git
 import yaml
 import copier
 import cloudpickle
-from git import Repo
 from shutil import rmtree
 from unittest.mock import patch
 from os import path, listdir, makedirs
@@ -12,22 +12,29 @@ import queue
 import time
 
 TEMPLATES_DIR = 'templates'
-VALID_YAMLS = [
-    'copier.yaml',
-    'copier.yml',
-]
+TEMPLATES_FILE = path.join(TEMPLATES_DIR, 'templates.yaml')
 
 log = logging.Logger('Copier')
 log.addHandler(logging.FileHandler('copier.log', mode='a'))
 log.setLevel(10)
 
 
-def get_copier_file(name: str) -> dict | None:
-    for filename in VALID_YAMLS:
-        filepath = path.join(TEMPLATES_DIR, name, filename)
-        if path.isfile(filepath):
+def load_yaml(file_path: str) -> dict|None:
+    base_path, file_name = path.split(path.normpath(file_path))
+    if not path.isdir(base_path):
+        return None
+    file_base, file_ext = path.splitext(file_name)
+
+    options = [file_name]
+    for ext in ('.yaml', '.yml'):
+        new_opt = path.join(base_path, file_base + ext)
+        if not new_opt in options:
+            options.append(new_opt)
+
+    for opt_path in options:
+        if path.isfile(opt_path):
             try:
-                with open(filepath, 'r') as file:
+                with open(opt_path, 'r') as file:
                     yam = yaml.safe_load(file)
             except Exception:
                 pass
@@ -36,18 +43,43 @@ def get_copier_file(name: str) -> dict | None:
     return None
 
 
-def get_templates() -> list[str]:
-    return [
-        name for name in listdir(TEMPLATES_DIR) \
-        if get_copier_file(name) is not None
-    ]
+def get_templates() -> dict:
+    return load_yaml(TEMPLATES_FILE) or {}
+
+
+def add_template(name, path, description) -> None:
+    new_template = {'path': path, 'description': description}
+    templates = get_templates()
+    templates[name] = new_template
+    with open(TEMPLATES_FILE, 'w') as file:
+        yaml.dump(templates, file)
+
 
 
 def clone_template(uri: str, name: str):
     dst_path = path.join(TEMPLATES_DIR, name)
     # if path.exists(dst_path):
     #     rmtree(dst_path)
-    Repo.clone_from(uri, dst_path)
+    git.Repo.clone_from(uri, dst_path)
+
+
+def get_template_path(template):
+    template_path = get_templates().get(template, {}).get('path')
+    if not template_path:
+        return None
+
+    elif not path.isabs(template_path) and load_yaml(path.join(TEMPLATES_DIR, template_path, 'copier.yaml')) is not None:
+        return path.join(TEMPLATES_DIR, template_path)
+
+    elif load_yaml(path.join(template_path, 'copier.yaml')) is not None:
+        return template_path
+
+    try:
+        git.cmd.Git().ls_remote(template_path)
+    except Exception:
+        return None
+    else:
+        return template_path
 
 
 class Generator():
@@ -58,6 +90,7 @@ class Generator():
         self._in_q = mp.Queue()
         self._out_q = mp.Queue()
 
+        self.template_path = get_template_path(self.template)
         self.proc = mp.Process(target = self._run_copy_proc)
         self.proc.start()
         self.current_question = None
@@ -126,7 +159,7 @@ class Generator():
             rmtree(self.dst_path)
         makedirs(self.dst_path, exist_ok = True)
         copier.run_copy(
-            src_path = path.join(TEMPLATES_DIR, self.template),
+            src_path = self.template_path,
             dst_path = self.dst_path,
             data = {key: val['answer'] for key, val in self.data.items()},
             overwrite = True,
@@ -143,10 +176,6 @@ class Generator():
     def _io_handler(self, in_queue, out_queue, questions, answers, **kwargs):
         question = questions[0]
         log.debug(f'NEW QUESTION: {question['name']}')
-        # if not question['when']('nya~'):
-        #     log.debug('skipping question')
-        #     return {}
-
         log.debug('writing to queue')
         out_queue.put((cloudpickle.dumps(question), None))
         log.debug('waiting for client reply')
@@ -173,7 +202,7 @@ class Generator():
         with patch('copier._main.unsafe_prompt', new = partial(self._io_handler, self._in_q, self._out_q)):
             log.debug('running copy...')
             copier.run_copy(
-                src_path = path.join(TEMPLATES_DIR, self.template),
+                src_path = self.template_path,
                 dst_path = self.dst_path,
                 overwrite = True,
                 quiet = True,
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import logging
 from os import path
 from pydoc import locate
 from typing import Literal
+import requests
 from fastmcp import FastMCP, Context
 from mcp.types import SamplingMessage, TextContent
 from fastmcp.tools.tool import ToolResult
@@ -19,6 +20,13 @@ mcp = FastMCP('copier-templates')
 generator = None
 
 
+def get_template_confirm_model(description: str):
+    return create_model('TemplateConfirmation', description = (str, Field(
+        description = f'Is the description ok like this?\n"{description}"',
+        default = description
+    )))
+
+
 def get_python_type(copier_type: str):
     # copier types: bool, float, int, json, path, str, yaml
     if copier_type in ('json', 'path', 'yaml', None):
@@ -32,12 +40,16 @@ def get_param_request_model(params: dict, to_revise: list):
     for key in to_revise:
         match params[key]['question']['type']:
             case 'select':
+                log.debug(f'select param: {key}')
                 fields[key] = (Literal[*params[key]['question']['choices']], Field(title = key, description = f'({params[key]['question'].get('message', '')})', default = params[key]['answer']))
             case 'checkbox':
+                log.debug(f'checkbox param: {key}')
                 fields[key] = (list[Literal[*params[key]['question']['choices']]], Field(title = key, description = f'({params[key]['question'].get('message', '')})', default = params[key]['answer']))
             case 'confirm':
+                log.debug(f'confirm param: {key}')
                 fields[key] = (bool, Field(title = key, description = f'({params[key]['question'].get('message', '')})', default = params[key]['answer']))
             case _:
+                log.debug(f'generic param: {key}')
                 fields[key] = (str, Field(title = key, description = f'({params[key]['question'].get('message', '')})', default = params[key]['answer']))
 
     return create_model('TemplateParameters', **fields)
@@ -46,7 +58,7 @@ def get_param_request_model(params: dict, to_revise: list):
 def get_next_question_output(question: dict, error: str|None = None) -> ToolResult:
     response_dict = {
         'next_question': question.get('message', question['name']).strip(),
-        'instructions': 'Call the `set_next_parameter` tool to submit a value.'
+        'instructions': 'Call the `set_next_parameter` tool to submit a value'
     }
     if 'choices' in question:
         response_dict['options'] = question.choices
@@ -64,12 +76,14 @@ def get_next_question_output(question: dict, error: str|None = None) -> ToolResu
 
 @mcp.tool
 async def set_next_parameter(ctx: Context, response: str|list[str]):
-    """Fill in a parameter for the template currently being generated and recieve the prompt for the next parameter.
-    Use this tool only after starting the generation with the `start_project` tool.
-    Returns the next template parameter to set by calling the `set_next_parameter` tool again.
+    """Fill in a parameter for the template currently being generated and receive the prompt for the next parameter
+    Use this tool only after starting the generation with the `start_project` tool
 
     Args:
         response: Value for the next template parameter
+
+    Returns:
+        Next template parameter to set by calling the `set_next_parameter` tool again
     """
     generator.respond(response)
     log.debug('getting next question')
@@ -121,12 +135,14 @@ async def set_next_parameter(ctx: Context, response: str|list[str]):
 
 @mcp.tool
 async def start_project(template: str, destination: str):
-    """Start generating a new projec using a given copier template. Use when asked to create a project from a template.
-    Returns the first template parameter to fill in using the `set_next_parameter` tool.
+    """Start generating a new projec using a given copier template
     
     Args:
-        template: Name of the template to use.
-        destination: Path to new project's directory.
+        template: Name of the template to use
+        destination: Path to new project's directory
+
+    Returns:
+        First template parameter to fill in using the `set_next_parameter` tool
     """
     global generator
     if generator is not None:
@@ -138,32 +154,69 @@ async def start_project(template: str, destination: str):
 
 
 @mcp.tool
-async def add_template(uri: str) -> str:
-    """Add a copier template to the list of available templates and returns its parameters.
-    Use this when given a git-repo or local path to use as template.
+async def search_github_for_templates(query: str) -> list[dict[str, str]]:
+    """Search Github for copier templates to add to the list of templates
+    Use this when no fitting template for a new project exists to find and add a template instead of writing a project from scratch
 
     Args:
-        uri: Path to local or remote Git-repo of the template to be added.
+        query: Github search query. Ideally less than 3 keywords
+
+    Returns:
+        List of at most 200 search results
     """
+    url = f'https://api.github.com/search/repositories?q=copier+template+{requests.utils.quote(query.strip())}'
+    response = requests.get(url)
+    response.raise_for_status()
+    results = response.json().get('items', [])
+    return [{
+        'name': repo.get('name', ''),
+        'url': repo.get('clone_url', ''),
+        'github-description': repo.get('description', '').strip()
+    } for repo in results[:200]]
+
+
+@mcp.tool
+async def add_template(ctx: Context, uri: str, description: str) -> str:
+    """Add a copier template to the list of available templates
+
+    Args:
+        uri: Path to local directory or remote Git-repo of the template to be added
+        description: A short text explaining the template and when it should be used (max 500 char)
+    """
+    if len(description) > 500:
+        raise ToolError(f'Description too long. Max 500 char')
 
     try:
         name = path.splitext(path.basename(path.normpath(uri)))[0]
     except Exception:
         raise ToolError(f'Invalid URI: {uri}')
 
-    try:
-        copier_utils.clone_template(uri, name)
-    except Exception:
-        raise ToolError(f'Failed to clone template repo: {uri}')
+    
+    confirm_response = await ctx.elicit(
+        message = f'Add {name!r} to copier template list? - URL: {uri}',
+        response_type = get_template_confirm_model(description)
+    )
+    if confirm_response.action == 'accept':
+        try:
+            copier_utils.add_template(name, uri, confirm_response.data.description or description)
+        except Exception:
+            raise ToolError(f'Failed to add template')
 
-    return f'Template is now available under the name `{name}`.'
+        return f'Template is now available under the name `{name}`'
+    else:
+        return 'Template rejected by user'
 
 
 @mcp.tool
-async def list_templates() -> list[str]:
-    """List available templates."""
-
-    return copier_utils.get_templates()
+async def list_templates() -> dict[str, str]:
+    """List available templates with descriptions
+    Use this when asked to create a new project and if a template matches the desired project specs use it with the `start_project` tool instead of coding from scratch
+    """
+    yam = copier_utils.get_templates()
+    res = {name: props.get('description') or '' for name, props in yam.items()}
+    log.debug(yam)
+    log.debug(res)
+    return res
 
 
 if __name__ == '__main__':
